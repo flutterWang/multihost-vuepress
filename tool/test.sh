b639fb183ae51e9e180f86f23fc8c38059157b52
Wbofeng <wangbofeng980404@gmail.com>
2020-10-24 01:47:11 +0800

    update shell

--- tool/test.sh
+++ tool/test.sh
@@ -1,1 +1,6 @@
-ls -l
+work_path=$(pwd)
+mkdir ../quick_vuepress && cd ../quick_vuepress
+npm init -y
+npm install -D vuepress
+mkdir docs && cd docs && cp ${work_path}/README.md ./
+mkdir .vuepress && cd .vuepress && cp ${work_path}/config.js ./
