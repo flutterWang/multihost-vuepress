035e869ee06aee81cc23378503ed63f13890ec10
flutterWang <wangbofeng980404@gmail.com>
2020-10-24 15:42:00 +0800

    uodate

--- config.js
+++ config.js
@@ -4,6 +4,15 @@ let config = JSON.parse(fs.readFileSync(file));
 
 let vuePressConfig = config.config
 
+vuePressConfig.themeConfig.nav.push(
+  {
+    text: "了解更多",
+    items: [
+      { text: "GOCN", link: "https://gocn.vip/" }
+    ],
+  }
+)
+
 module.exports = {
   title: vuePressConfig.title,
   description: vuePressConfig.description,
